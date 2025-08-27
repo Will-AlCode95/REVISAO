27b07a4be713aaf2469241aedfbcd2090fa030ea
will <will@gmail.com>
2025-08-26 22:29:32 -0300

    ok

---
+++ atividade.py
@@ -0,0 +1,15 @@
+palavra = (input("digite a sentença: "))
+
+def diga_correto():
+
+ if palavra == "IRINEUSKY":
+    print("Irineusky is the BEST plant EVER!")
+ elif palavra == "irineusky":
+    print("No! I want A UPPER BIG IRINEUSKY!")
+ else:
+    print(f"irineusky! not {palavra}")
+ return palavra
+
+diga_correto()
+
+
---
+++ atividade3.py
@@ -0,0 +1,13 @@
+x = 11
+y = 4
+
+x = x % y
+x = x % y
+y = y % x
+
+print(y)
+
+
+x = input()
+y = int(input())
+print(x * y)
