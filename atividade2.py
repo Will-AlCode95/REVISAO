ac7b494f83dcc8999b050f543b55b990a8723c89
Bill <williamvinicius007@gmail.com>
2025-08-27 12:06:30 -0300

    blz

--- atividade2.py
+++ atividade2.py
@@ -3,9 +3,10 @@ income = float(input("Enter the annual income: "))
 if income < 85528:
 	tax = income * 0.18 - 556.02
 else:
- income > 85528
- tax = 14.839 * 0.32 # Write the rest of your code here.
+	tax = (income - 85528) * 0.32 + 14839.02
+
+if tax < 0.0:
+	tax = 0.0
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
- 
